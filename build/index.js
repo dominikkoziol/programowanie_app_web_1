b3187587148f381f2e0ad246ea95b5fddec648f6
Dominik Kozioł <dkoziol097@gmail.com>
2021-03-07 12:49:12 +0100

    progress

--- build/index.js
+++ build/index.js
@@ -3,37 +3,53 @@ var StatsApp = /** @class */ (function () {
         this.initApp();
     }
     StatsApp.prototype.initApp = function () {
-        this.getFields();
-        this.watchInputs();
+        // this.getFields();
+        // this.watchInputs();
     };
-    StatsApp.prototype.getFields = function () {
-        this.inputData1 = document.querySelector("#data1");
-        this.inputData2 = document.querySelector("#data2");
-        this.inputData3 = document.querySelector("#data3");
-        this.inputData4 = document.querySelector("#data4");
-        this.outputData1 = document.querySelector("#output1");
-        this.outputData2 = document.querySelector("#output2");
-        this.outputData3 = document.querySelector("#output3");
-        this.outputData4 = document.querySelector("#output4");
-    };
-    StatsApp.prototype.watchInputs = function () {
+    StatsApp.prototype.getAndWatchDynamicField = function () {
         var _this = this;
-        this.inputData1.addEventListener('input', function () { return _this.calculateAndShowData(); });
-        this.inputData2.addEventListener('input', function () { return _this.calculateAndShowData(); });
-        this.inputData3.addEventListener('input', function () { return _this.calculateAndShowData(); });
-        this.inputData4.addEventListener('input', function () { return _this.calculateAndShowData(); });
+        this.fieldsNumber = document.querySelector("#fieldsnumber");
+        this.fieldsNumber.addEventListener("input", function () { return _this.watchFieldsNumber; });
     };
-    StatsApp.prototype.calculateAndShowData = function () {
-        var input1 = +this.inputData1.value;
-        var input2 = +this.inputData2.value;
-        var input3 = +this.inputData3.value;
-        var input4 = +this.inputData4.value;
-        var sum = input1 + input2 + input3 + input4;
-        var avg = sum / 4;
-        var min = Math.min(input1, input2, input3, input4);
-        var max = Math.max(input1, input2, input3, input4);
-        this.showData(sum, avg, min, max);
+    StatsApp.prototype.watchFieldsNumber = function () {
+        this.generateFields(+this.fieldsNumber.value);
     };
+    StatsApp.prototype.generateFields = function (fieldsNumber) {
+        for (var i = 0; i < fieldsNumber; i++) {
+            var div = document.createElement("div");
+            var label = document.createElement("label");
+            div.classList.add("form-field");
+            label.setAttribute("for", "data" + i);
+            div.appendChild(label);
+        }
+    };
+    // public getFields(): void {
+    //     this.inputData1 = document.querySelector("#data1");
+    //     this.inputData2 = document.querySelector("#data2");
+    //     this.inputData3 = document.querySelector("#data3");
+    //     this.inputData4 = document.querySelector("#data4");
+    //     this.outputData1 = document.querySelector("#output1");
+    //     this.outputData2 = document.querySelector("#output2");
+    //     this.outputData3 = document.querySelector("#output3");
+    //     this.outputData4 = document.querySelector("#output4");
+    // }
+    // public watchInputs(): void {
+    //     this.inputData1.addEventListener('input', () => this.calculateAndShowData());
+    //     this.inputData2.addEventListener('input', () => this.calculateAndShowData());
+    //     this.inputData3.addEventListener('input', () => this.calculateAndShowData());
+    //     this.inputData4.addEventListener('input', () => this.calculateAndShowData());
+    // }
+    // public calculateAndShowData(): void {
+    //     const input1 = +this.inputData1.value;
+    //     const input2 = +this.inputData2.value;
+    //     const input3 = +this.inputData3.value;
+    //     const input4 = +this.inputData4.value;
+    //     const sum = input1 + input2 + input3 + input4;
+    //     const avg = sum / 4;
+    //     const min = Math.min(input1, input2, input3, input4);
+    //     const max = Math.max(input1, input2, input3, input4);
+    //     this.showData(sum, avg, min, max);
+    // }
     StatsApp.prototype.showData = function (sum, avg, min, max) {
         this.outputData1.value = sum.toString();
         this.outputData2.value = avg.toString();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 class StatsApp {
+    //how many inputs field
+    public fieldsNumber: HTMLInputElement;
 
     //inputs
     public inputData1: HTMLInputElement;
@@ -17,44 +19,65 @@ class StatsApp {
     }
 
     public initApp(): void {
-        this.getFields();
-        this.watchInputs();
+        // this.getFields();
+        // this.watchInputs();
+    }
+
+    public getAndWatchDynamicField(): void {
+        this.fieldsNumber = document.querySelector("#fieldsnumber");
+        this.fieldsNumber.addEventListener("input", () => this.watchFieldsNumber);
+    }
+
+    public watchFieldsNumber(): void {
+        this.generateFields(+this.fieldsNumber.value);
+    }
+
+    public generateFields(fieldsNumber: number): void {
+        for(var i = 0; i < fieldsNumber; i++) {
+            const div = document.createElement("div");
+            const label = document.createElement("label")
+            
+
+            div.classList.add("form-field");
+            label.setAttribute("for", "data" + i);
+            div.appendChild(label);
+        }
     }
 
 
-    public getFields(): void {
-        this.inputData1 = document.querySelector("#data1");
-        this.inputData2 = document.querySelector("#data2");
-        this.inputData3 = document.querySelector("#data3");
-        this.inputData4 = document.querySelector("#data4");
+    // public getFields(): void {
+    //     this.inputData1 = document.querySelector("#data1");
+    //     this.inputData2 = document.querySelector("#data2");
+    //     this.inputData3 = document.querySelector("#data3");
+    //     this.inputData4 = document.querySelector("#data4");
 
-        this.outputData1 = document.querySelector("#output1");
-        this.outputData2 = document.querySelector("#output2");
-        this.outputData3 = document.querySelector("#output3");
-        this.outputData4 = document.querySelector("#output4");
-    }
+    //     this.outputData1 = document.querySelector("#output1");
+    //     this.outputData2 = document.querySelector("#output2");
+    //     this.outputData3 = document.querySelector("#output3");
+    //     this.outputData4 = document.querySelector("#output4");
+    // }
 
 
-    public watchInputs(): void {
-        this.inputData1.addEventListener('input', () => this.calculateAndShowData());
-        this.inputData2.addEventListener('input', () => this.calculateAndShowData());
-        this.inputData3.addEventListener('input', () => this.calculateAndShowData());
-        this.inputData4.addEventListener('input', () => this.calculateAndShowData());
-    }
+    // public watchInputs(): void {
+    //     this.inputData1.addEventListener('input', () => this.calculateAndShowData());
+    //     this.inputData2.addEventListener('input', () => this.calculateAndShowData());
+    //     this.inputData3.addEventListener('input', () => this.calculateAndShowData());
+    //     this.inputData4.addEventListener('input', () => this.calculateAndShowData());
+    // }
 
-    public calculateAndShowData(): void {
-        const input1 = +this.inputData1.value;
-        const input2 = +this.inputData2.value;
-        const input3 = +this.inputData3.value;
-        const input4 = +this.inputData4.value;
+    // public calculateAndShowData(): void {
+    //     const input1 = +this.inputData1.value;
+    //     const input2 = +this.inputData2.value;
+    //     const input3 = +this.inputData3.value;
+    //     const input4 = +this.inputData4.value;
 
-        const sum = input1 + input2 + input3 + input4;
-        const avg = sum / 4;
-        const min = Math.min(input1, input2, input3, input4);
-        const max = Math.max(input1, input2, input3, input4);
-
-        this.showData(sum, avg, min, max);
-    }
+    //     const sum = input1 + input2 + input3 + input4;
+    //     const avg = sum / 4;
+    //     const min = Math.min(input1, input2, input3, input4);
+    //     const max = Math.max(input1, input2, input3, input4);
+        
+    //     this.showData(sum, avg, min, max);
+    // }
 
     public showData(sum: number, avg: number, min: number, max: number): void {
         this.outputData1.value = sum.toString();

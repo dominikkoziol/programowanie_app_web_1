0129cc30b3465962a49bb7c61371a685eca3dcc0
Dominik Kozioł <dkoziol097@gmail.com>
2021-03-14 11:57:02 +0100

    finish extension

--- build/index.js
+++ build/index.js
@@ -5,51 +5,78 @@ var StatsApp = /** @class */ (function () {
     StatsApp.prototype.initApp = function () {
         // this.getFields();
         // this.watchInputs();
+        this.getAndWatchDynamicField();
+        this.getOutputFields();
     };
     StatsApp.prototype.getAndWatchDynamicField = function () {
         var _this = this;
         this.fieldsNumber = document.querySelector("#fieldsnumber");
-        this.fieldsNumber.addEventListener("input", function () { return _this.watchFieldsNumber; });
+        this.fieldsNumber.addEventListener("input", function () { return _this.watchFieldsNumber(); });
     };
     StatsApp.prototype.watchFieldsNumber = function () {
         this.generateFields(+this.fieldsNumber.value);
     };
     StatsApp.prototype.generateFields = function (fieldsNumber) {
-        for (var i = 0; i < fieldsNumber; i++) {
+        var _this = this;
+        var inputsContainer = document.querySelector("#inputs");
+        inputsContainer.innerHTML = "";
+        var _loop_1 = function () {
             var div = document.createElement("div");
             var label = document.createElement("label");
+            var input = document.createElement("input");
+            var button = document.createElement("button");
             div.classList.add("form-field");
             label.setAttribute("for", "data" + i);
+            label.innerHTML = "Data " + i;
             div.appendChild(label);
+            input.setAttribute("id", "data" + i);
+            input.classList.add("data");
+            input.addEventListener("input", function () { return _this.calculateAndShowData(); });
+            button.innerHTML = "Clear";
+            button.addEventListener("click", function () {
+                input.value = "";
+                _this.calculateAndShowData();
+            });
+            div.appendChild(input);
+            div.appendChild(button);
+            inputsContainer.appendChild(div);
+        };
+        for (var i = 0; i < fieldsNumber; i++) {
+            _loop_1();
         }
+        var outputs = document.querySelector("#outputs");
+        outputs.style.display = "flex";
     };
-    // public getFields(): void {
-    //     this.inputData1 = document.querySelector("#data1");
-    //     this.inputData2 = document.querySelector("#data2");
-    //     this.inputData3 = document.querySelector("#data3");
-    //     this.inputData4 = document.querySelector("#data4");
-    //     this.outputData1 = document.querySelector("#output1");
-    //     this.outputData2 = document.querySelector("#output2");
-    //     this.outputData3 = document.querySelector("#output3");
-    //     this.outputData4 = document.querySelector("#output4");
-    // }
+    StatsApp.prototype.getOutputFields = function () {
+        // this.inputData1 = document.querySelector("#data1");
+        // this.inputData2 = document.querySelector("#data2");
+        // this.inputData3 = document.querySelector("#data3");
+        // this.inputData4 = document.querySelector("#data4");
+        this.outputData1 = document.querySelector("#output1");
+        this.outputData2 = document.querySelector("#output2");
+        this.outputData3 = document.querySelector("#output3");
+        this.outputData4 = document.querySelector("#output4");
+    };
     // public watchInputs(): void {
     //     this.inputData1.addEventListener('input', () => this.calculateAndShowData());
     //     this.inputData2.addEventListener('input', () => this.calculateAndShowData());
     //     this.inputData3.addEventListener('input', () => this.calculateAndShowData());
     //     this.inputData4.addEventListener('input', () => this.calculateAndShowData());
     // }
-    // public calculateAndShowData(): void {
-    //     const input1 = +this.inputData1.value;
-    //     const input2 = +this.inputData2.value;
-    //     const input3 = +this.inputData3.value;
-    //     const input4 = +this.inputData4.value;
-    //     const sum = input1 + input2 + input3 + input4;
-    //     const avg = sum / 4;
-    //     const min = Math.min(input1, input2, input3, input4);
-    //     const max = Math.max(input1, input2, input3, input4);
-    //     this.showData(sum, avg, min, max);
-    // }
+    StatsApp.prototype.calculateAndShowData = function () {
+        var values = [];
+        var sum = 0;
+        var data = document.querySelectorAll(".data");
+        for (var i = 0; i < +this.fieldsNumber.value; i++) {
+            var input = data[i];
+            values.push(+input.value);
+            sum += +input.value;
+        }
+        var avg = sum / +this.fieldsNumber.value;
+        var min = Math.min.apply(Math, values);
+        var max = Math.max.apply(Math, values);
+        this.showData(sum, avg, min, max);
+    };
     StatsApp.prototype.showData = function (sum, avg, min, max) {
         this.outputData1.value = sum.toString();
         this.outputData2.value = avg.toString();
--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,13 @@ class StatsApp {
     public initApp(): void {
         // this.getFields();
         // this.watchInputs();
+        this.getAndWatchDynamicField();
+        this.getOutputFields();
     }
 
     public getAndWatchDynamicField(): void {
         this.fieldsNumber = document.querySelector("#fieldsnumber");
-        this.fieldsNumber.addEventListener("input", () => this.watchFieldsNumber);
+        this.fieldsNumber.addEventListener("input", () => this.watchFieldsNumber());
     }
 
     public watchFieldsNumber(): void {
@@ -33,29 +35,49 @@ class StatsApp {
     }
 
     public generateFields(fieldsNumber: number): void {
+        const inputsContainer = document.querySelector("#inputs");
+        inputsContainer.innerHTML = "";
         for(var i = 0; i < fieldsNumber; i++) {
             const div = document.createElement("div");
             const label = document.createElement("label")
-            
+            const input = document.createElement("input");
+            const button = document.createElement("button");
 
             div.classList.add("form-field");
             label.setAttribute("for", "data" + i);
+            label.innerHTML = "Data " + i; 
             div.appendChild(label);
+            input.setAttribute("id", "data" + i);
+            input.classList.add("data");
+            input.addEventListener("input", () => this.calculateAndShowData());
+            button.innerHTML = "Clear";
+            button.addEventListener("click", () => {
+                input.value = "";
+                this.calculateAndShowData();
+            });
+            div.appendChild(input);
+            div.appendChild(button);
+
+
+            inputsContainer.appendChild(div);         
         }
+
+       const outputs: HTMLDivElement = document.querySelector("#outputs");
+       outputs.style.display = "flex";
     }
 
 
-    // public getFields(): void {
-    //     this.inputData1 = document.querySelector("#data1");
-    //     this.inputData2 = document.querySelector("#data2");
-    //     this.inputData3 = document.querySelector("#data3");
-    //     this.inputData4 = document.querySelector("#data4");
+    public getOutputFields(): void {
+        // this.inputData1 = document.querySelector("#data1");
+        // this.inputData2 = document.querySelector("#data2");
+        // this.inputData3 = document.querySelector("#data3");
+        // this.inputData4 = document.querySelector("#data4");
 
-    //     this.outputData1 = document.querySelector("#output1");
-    //     this.outputData2 = document.querySelector("#output2");
-    //     this.outputData3 = document.querySelector("#output3");
-    //     this.outputData4 = document.querySelector("#output4");
-    // }
+        this.outputData1 = document.querySelector("#output1");
+        this.outputData2 = document.querySelector("#output2");
+        this.outputData3 = document.querySelector("#output3");
+        this.outputData4 = document.querySelector("#output4");
+    }
 
 
     // public watchInputs(): void {
@@ -65,19 +87,23 @@ class StatsApp {
     //     this.inputData4.addEventListener('input', () => this.calculateAndShowData());
     // }
 
-    // public calculateAndShowData(): void {
-    //     const input1 = +this.inputData1.value;
-    //     const input2 = +this.inputData2.value;
-    //     const input3 = +this.inputData3.value;
-    //     const input4 = +this.inputData4.value;
-
-    //     const sum = input1 + input2 + input3 + input4;
-    //     const avg = sum / 4;
-    //     const min = Math.min(input1, input2, input3, input4);
-    //     const max = Math.max(input1, input2, input3, input4);
+    public calculateAndShowData(): void {
+        let values: number[] = [];
+        let sum = 0;
+        const data = document.querySelectorAll(".data");
+      
+        for(let i = 0; i < +this.fieldsNumber.value; i++) {
+            var input = <HTMLInputElement>data[i];
+            values.push(+input.value);
+            sum += +input.value;
+        }   
+   
+        const avg = sum / +this.fieldsNumber.value;
+        const min = Math.min(...values);
+        const max = Math.max(...values);
         
-    //     this.showData(sum, avg, min, max);
-    // }
+        this.showData(sum, avg, min, max);
+    }
 
     public showData(sum: number, avg: number, min: number, max: number): void {
         this.outputData1.value = sum.toString();
